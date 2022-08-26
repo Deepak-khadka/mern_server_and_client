1132dfb060b895f69835f06b8bf3fe662066c66c
Deepak <rozankhadka159@gmail.com>
2022-08-26 19:07:08 +0545

    chore: include category controller

--- server/Routes/category.js
+++ server/Routes/category.js
@@ -1,7 +1,8 @@
 import express from "express";
-import { addCategory } from "../controller/category.js";
+import { addCategory, categoryList } from "../controller/category.js";
 const router = express.Router();
 
+router.get("/", categoryList);
 router.post("/create", addCategory);
 
 export default router;
--- server/controller/category.js
+++ server/controller/category.js
@@ -1,8 +1,20 @@
 import CategoryModel from "../models/Category.js";
 import slugify from "slugify";
 
+/** Retrieve all the category list  */
+
+export const categoryList = (req, res) => {
+  CategoryModel.find({}, (err, result) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json(result);
+    }
+  });
+};
+
+/* Create a category object where slug can be auto generated according to their name */
 export const addCategory = async (req, res) => {
-  /* Create a category object where slug can be auto generated according to their name */
   const category = {
     name: req.body.name,
     slug: slugify(req.body.name),
@@ -18,4 +30,26 @@ export const addCategory = async (req, res) => {
       return res.status(201).json({ category });
     }
   });
+};
+
+/* Delete the category by its id  */
+export const deleteCategory = async (req, res) => {
+  const categoryId = req.params.id;
+
+  await CategoryModel.deleteOne({ _id: categoryId });
+
+  res.send().json({ message: "Category Deleted successfully " });
+};
+
+/** To Update the category by it's id */
+export const updateCategory = async (request, response) => {
+  const id = request.params.id;
+
+  const category = await CategoryModel.findOneAndUpdate(
+    { _id: id },
+    { $set: request.body },
+    { new: true }
+  );
+
+  response.status(201).json({ category });
 };
